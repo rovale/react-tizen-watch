a87f8f02a6516f320ebae13b1c2bfd459005d432
Ronald van Leuwen <rvleuwen@gmail.com>
2017-04-27 14:47:03 +0200

    Adds some basic navigation.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,69 @@
 import './tau/wearable/theme/default/tau.css'
 import './tau/wearable/theme/default/tau.circle.css'
+import './tau/wearable/js/tau.min.js'
 
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentPage: "main"
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('tizenhwkey', ev => {
+      if (ev.keyName === "back") {
+        if (this.state.currentPage === "main") {
+          try {
+            window.tizen.application.getCurrentApplication().exit();
+          } catch (ignore) { }
+        } else {
+          this.changePage("main");          
+        }
+      }
+    });
+  }
+
+  changePage(page) {
+    this.setState({
+      currentPage: page
+    });
+  }
+
   render() {
-    return (
-      <div className="ui-page ui-page-active" id="main">
-        <header className="ui-header">
-          <h2 className="ui-title">React Tizen</h2>
-        </header>
-        <div className="ui-content">
-          To get started, edit <code>src/App.js</code> and save to reload.
+    if (this.state.currentPage === "main") {
+      return (
+        <div className="ui-page ui-page-active">
+          <header className="ui-header">
+            <h2 className="ui-title">React Tizen</h2>
+          </header>
+          <div className="ui-content">
+            <ul className="ui-listview">
+              <li><a href="#" onClick={() => this.changePage("option1")}>Option 1</a></li>
+              <li><a href="#" onClick={() => this.changePage("option2")}>Option 2</a></li>
+              <li><a href="#" onClick={() => this.changePage("option3")}>Option 3</a></li>
+            </ul>
+          </div>
+          <div className="ui-processing ui-processing-full-size"></div>
         </div>
-        <div className="ui-processing ui-processing-full-size"></div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="ui-page ui-page-active">
+          <header className="ui-header">
+            <h2 className="ui-title">React Tizen</h2>
+          </header>
+          <div className="ui-content">
+            <ul className="ui-listview">
+              Current page: {this.state.currentPage}
+            </ul>
+          </div>
+        </div>
+      );      
+    }
   }
 }
 
